ce746355236ea5ecdbef883595d70937c25f182b
goblin <zhangyl2015@qq.com>
2022-03-22 18:35:40 +0800

    init

---
+++ example/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type binds []string
+
+func (b binds) String() string {
+	return strings.Join(b, ",")
+}
+
+func (b *binds) Set(v string) error {
+	*b = strings.Split(v, ",")
+	return nil
+}
+
+type Size interface {
+	Size() int64
+}
+
+// 生成随机数
+func generatePRData(l int) []byte {
+	res := make([]byte, l)
+	seed := uint64(1)
+	for i := 0; i < l; i++ {
+		seed = seed * 48271 % 2147483647
+		res[i] = byte(seed)
+	}
+	return res
+}
+
+func setupHandler(www string) http.Handler {
+	mux := http.NewServeMux()
+	if len(www) > 0 {
+		mux.Handle("/", http.FileServer(http.Dir(www)))
+	} else {
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("%#v\n", r)
+			const maxSize = 1 << 30 // 1GB
+			num, err := strconv.ParseInt(strings.ReplaceAll(r.RequestURI, "/", ""), 10, 64)
+			if err != nil || num <= 0 || num > maxSize {
+				w.WriteHeader(400)
+				return
+			}
+			w.Write(generatePRData(int(num)))
+		})
+	}
+
+	mux.HandleFunc("/demo/tiles", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><head><style>img{width:40px;height:40px;}</style></head><body>")
+		for i := 0; i < 200; i++ {
+			fmt.Fprintf(w, `<img src="/demo/tile?cachebust=%d">`, i)
+		}
+		io.WriteString(w, "</body></html>")
+	})
+
+	mux.HandleFunc("/demo/echo", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Printf("error reading body while handling /echo: %s\n", err.Error())
+		}
+		w.Write(body)
+	})
+
+	// accept file uploads and return the MD5 of the uploaded file
+	// maximum accepted file size is 1 GB
+	mux.HandleFunc("/demo/upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			err := r.ParseMultipartForm(1 << 30) // 1 GB
+			if err == nil {
+				var file multipart.File
+				file, _, err = r.FormFile("uploadfile")
+				if err == nil {
+					var size int64
+					if sizeInterface, ok := file.(Size); ok {
+						size = sizeInterface.Size()
+						b := make([]byte, size)
+						file.Read(b)
+						md5 := md5.Sum(b)
+						fmt.Fprintf(w, "%x", md5)
+						return
+					}
+					err = errors.New("couldn't get uploaded file size")
+				}
+			}
+			utils.DefaultLogger.Infof("Error receiving upload: %#v", err)
+		}
+		io.WriteString(w, `<html><body><form action="/demo/upload" method="post" enctype="multipart/form-data">
+				<input type="file" name="uploadfile"><br>
+				<input type="submit">
+			</form></body></html>`)
+	})
+
+	return mux
+
+}
